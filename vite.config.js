5ffe6a310fcfdd08b8e487288e1e8e535ea79df5
niney <kpeteras@gmail.com>
2025-12-31 20:07:44 +0900

    "vite-plugin-checker 플러그인 추가 및 타입스크립트 검사 기능 활성화"

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import checker from 'vite-plugin-checker'
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    checker({ typescript: true })
+  ],
   base: '/foodict-page/',
   server: {
     host: true
