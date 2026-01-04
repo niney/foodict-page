43658079131fdcf01339dfc5c7a711902a70629d
niney <kpeteras@gmail.com>
2026-01-04 16:04:43 +0900

    흑백요리사 > 레스토랑 상세 > 임성근 국가공인 진갈비 셰프 정보 및 설명 수정

--- src/lib/data/restaurants/임성근국가공인진갈비.ts
+++ src/lib/data/restaurants/임성근국가공인진갈비.ts
@@ -4,7 +4,7 @@ const data: Restaurant = {
   id: 8,
   name: '임성근 국가공인 진갈비',
   nameEn: 'Im Sung Geun Galbi',
-  chef: '임성근',
+  chef: '임성근(모델)',
   chefNickname: null,
   spoonType: 'white',
   category: '갈비',
@@ -13,7 +13,7 @@ const data: Restaurant = {
   placeId: null,
   searchName: '임성근 국가공인 진갈비',
   image: 'https://images.unsplash.com/photo-1544025162-d76694265947?w=600',
-  description: '대한민국 조리기능장의 정통 갈비 전문점',
+  description: '대한민국 조리기능장의 정통 갈비 전문점 (임성근 셰프는 과거 \'국가공인 진갈비\' 브랜드와 모델 및 메뉴 자문 관계로 연관이 있었으나, 현재는 관계가 없음)',
   signature: ['진갈비', '양념갈비'],
   priceRange: '$$$',
 };
